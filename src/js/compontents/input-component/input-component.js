8908903731feca0862c8504c98fcf65118760950
Jonathan Flores <jf223mu@student.lnu.se>
2023-10-10 15:38:58 +0200

    Add module test

--- src/js/compontents/input-component/input-component.js
+++ src/js/compontents/input-component/input-component.js
@@ -1,3 +1,4 @@
+import { MyChart } from 'testgraphifyjs'
 /**
  * The virtual-window component.
  *
@@ -10,19 +11,63 @@ const template = document.createElement('template')
 template.innerHTML = `
 <style>
   body {
-    background-color: black;
+    background-color: #f4f4f4;
+  }
+
+  #barCanvas {
+    width: 400px;
+    height: 300px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+  }
+
+  h1 {
+    color: #333;
+  }
+
+  input {
+    padding: 10px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    width: 250px;
+  }
+
+  button {
+    margin-left: 10px;
+    padding: 10px 15px;
+    background-color: #007BFF;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.2s;
+  }
+
+  button:hover {
+    background-color: #0056b3;
+  }
+
+  .error-message {
+    color: red;
+    margin-top: 5px;
+    display: none;
   }
 
 </style>
 <div>
-  <h1> lmao </h1>
+  <h1>Input Statistics</h1>
+  <input type="text" id="statInput" placeholder="Enter comma-separated numbers e.g. 10,20,30">
+  <button id="generateGraph">Generate Graph</button>
+  <p id="error-message">Invalid input. Please enter comma-separated numbers.</p>
+  <canvas id="barCanvas"></canvas>
 </div>
 `
 
 /*
  * Define custom element.
  */
-customElements.define('input-component',
+customElements.define(
+  'input-component',
   /**
    * A virtual window
    */
@@ -37,6 +82,49 @@ customElements.define('input-component',
       // append the template to the shadow root.
       const shadowRoot = this.attachShadow({ mode: 'open' })
       shadowRoot.appendChild(template.content.cloneNode(true))
+
+      this.inputElement = shadowRoot.querySelector('#statInput')
+      this.generateGraphButton = shadowRoot.querySelector('#generateGraph')
+      this.barCanvas = shadowRoot.querySelector('#barCanvas')
+      this.errorMessage = shadowRoot.querySelector('#error-message')
+
+      this.generateGraphButton.addEventListener('click', () => {
+        this.generateGraph()
+      })
+    }
+
+    /**
+     *
+     */
+    generateGraph () {
+      const value = this.inputElement.value.split(',').map(num => parseInt(num, 10))
+
+      if (!this.validateInput(this.inputElement.value)) {
+        this.errorMessage.style.display = 'block'
+        return
+      } else {
+        this.errorMessage.style.display = 'none'
+      }
+
+      const barCtx = this.barCanvas.getContext('2d')
+      const barConfig = {
+        type: 'bar',
+        data: value,
+        labels: Array.from({ length: value.length }, (_, i) => `Label ${i + 1}`),
+        color: 'blue'
+      }
+
+      const barChart = new MyChart(barCtx, barConfig).init()
+      barChart.draw()
+      barChart.toggleGrid(true)
+    }
+
+    /**
+     *
+     * @param value
+     */
+    validateInput (value) {
+      return value.split(',').every(num => !isNaN(num))
     }
   }
 )
