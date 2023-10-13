12b061cfc9a319d48bf0aad86174325400206781
Jonathan Flores <jf223mu@student.lnu.se>
2023-10-13 11:20:36 +0200

    Add download function

--- src/js/compontents/input-component/input-component.js
+++ src/js/compontents/input-component/input-component.js
@@ -1,12 +1,5 @@
 import { MyChart } from 'testgraphifyjs'
-/**
- * The virtual-window component.
- *
- */
 
-/**
- * Define template.
- */
 const template = document.createElement('template')
 template.innerHTML = `
 <style>
@@ -15,25 +8,23 @@ template.innerHTML = `
   }
 
   #barCanvas {
-    width: 400px;
-    height: 300px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    border-radius: 8px;
+    transform: scale(1);
+    max-width: 100%;
+    border: 1px solid #ccc;
   }
 
   h1 {
     color: #333;
+    text-align: center;
   }
 
-  input {
+  input, select {
     padding: 10px;
     border-radius: 4px;
     border: 1px solid #ccc;
-    width: 250px;
   }
 
   button {
-    margin-left: 10px;
     padding: 10px 15px;
     background-color: #007BFF;
     color: white;
@@ -50,56 +41,117 @@ template.innerHTML = `
   .error-message {
     color: red;
     margin-top: 5px;
+    text-align: center;
     display: none;
   }
 
+  .stats-container {
+    text-align: center;
+  }
+
+  .single-stat {
+  }
+
+  .add-stat-btn {
+    display: block;
+    margin: 10px auto; 
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+
+  }
+
 </style>
+
 <div>
   <h1>Input Statistics</h1>
-  <input type="text" id="statInput" placeholder="Enter comma-separated numbers e.g. 10,20,30">
+  <select id="chartType">
+    <option value="bar">Bar Chart</option>
+    <option value="pie">Pie Chart</option>
+  </select>
+  <div class="stats-container">
+    <div class="single-stat">
+      <input type="text" placeholder="Enter a number e.g. 10">
+    </div>
+  </div>
+  <button class="add-stat-btn">+</button>
   <button id="generateGraph">Generate Graph</button>
-  <p id="error-message">Invalid input. Please enter comma-separated numbers.</p>
+  <p id="error-message">Invalid input. Please enter numbers.</p>
   <canvas id="barCanvas"></canvas>
+  <button id="downloadPNG">Download as PNG</button>
 </div>
 `
 
-/*
- * Define custom element.
+/**
+ * Custom web component for input and generation of charts.
+ *
+ * @extends {HTMLElement}
  */
-customElements.define(
-  'input-component',
+customElements.define('input-component', class InputComponent extends HTMLElement {
   /**
-   * A virtual window
+   * Constructor for the custom element.
    */
-  class InputComponent extends HTMLElement {
-    /**
-     * Creates an instance of the current type.
-     */
     constructor () {
       super()
-
-      // Attach a shadow DOM tree to this element and
-      // append the template to the shadow root.
+      // Attach shadow DOM and append template content
       const shadowRoot = this.attachShadow({ mode: 'open' })
       shadowRoot.appendChild(template.content.cloneNode(true))
 
-      this.inputElement = shadowRoot.querySelector('#statInput')
+      // Initialize element references
+      this.chartTypeElement = shadowRoot.querySelector('#chartType')
+      this.statsContainer = shadowRoot.querySelector('.stats-container')
       this.generateGraphButton = shadowRoot.querySelector('#generateGraph')
+      this.addStatButton = shadowRoot.querySelector('.add-stat-btn')
       this.barCanvas = shadowRoot.querySelector('#barCanvas')
       this.errorMessage = shadowRoot.querySelector('#error-message')
+      this.downloadPNGButton = shadowRoot.querySelector('#downloadPNG')
 
+      // Add event listeners
       this.generateGraphButton.addEventListener('click', () => {
         this.generateGraph()
+      })
+
+      this.addStatButton.addEventListener('click', () => {
+        this.addStatInput()
+      })
+
+      this.downloadPNGButton.addEventListener('click', () => {
+        this.downloadGraphAsPNG()
       })
     }
 
     /**
-     *
+     * Adds a new stat input field to the component.
+     */
+    addStatInput () {
+      const inputWrapper = document.createElement('div')
+      inputWrapper.classList.add('single-stat')
+      const inputField = document.createElement('input')
+      inputField.type = 'text'
+      inputField.placeholder = 'Enter a number e.g. 10'
+      inputWrapper.appendChild(inputField)
+      this.statsContainer.appendChild(inputWrapper)
+    }
+
+    /**
+     * Generates the graph based on user input.
      */
     generateGraph () {
-      const value = this.inputElement.value.split(',').map(num => parseInt(num, 10))
+      const inputs = this.statsContainer.querySelectorAll('input')
+      const values = []
+      inputs.forEach(input => {
+        const value = parseInt(input.value, 10)
+        if (!isNaN(value)) {
+          values.push(value)
+        }
+      })
 
-      if (!this.validateInput(this.inputElement.value)) {
+      if (values.length === 0 || values.length !== inputs.length) {
         this.errorMessage.style.display = 'block'
         return
       } else {
@@ -107,24 +159,38 @@ customElements.define(
       }
 
       const barCtx = this.barCanvas.getContext('2d')
+      const selectedChartType = this.chartTypeElement.value
       const barConfig = {
-        type: 'bar',
-        data: value,
-        labels: Array.from({ length: value.length }, (_, i) => `Label ${i + 1}`),
+        type: selectedChartType,
+        data: values,
+        labels: Array.from({ length: values.length }, (_, i) => `Label ${i + 1}`),
         color: 'blue'
       }
 
-      const barChart = new MyChart(barCtx, barConfig).init()
-      barChart.draw()
-      barChart.toggleGrid(true)
+      const chart = new MyChart(barCtx, barConfig).init()
+      chart.draw()
+      chart.toggleGrid(true)
     }
 
     /**
+     * Validates the input value to ensure it's a number.
      *
-     * @param value
+     * @param {string} value - The value to validate.
+     * @returns {boolean} - Returns true if value is a number, else false.
      */
     validateInput (value) {
-      return value.split(',').every(num => !isNaN(num))
+      return !isNaN(parseInt(value, 10))
     }
-  }
-)
+
+    /**
+     * Downloads the generated graph as a PNG image.
+     */
+    downloadGraphAsPNG () {
+      const format = 'image/png'
+      const image = this.barCanvas.toDataURL(format)
+      const link = document.createElement('a')
+      link.href = image
+      link.download = `graph.${format.split('/')[1]}`
+      link.click()
+    }
+  })
